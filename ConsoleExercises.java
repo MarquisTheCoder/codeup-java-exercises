153a845efe725634a4fd9773a237e8631c904252
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2022-10-03 10:16:36 -0500

    autosave script

--- ConsoleExercises.java
+++ ConsoleExercises.java
@@ -12,8 +12,5 @@ public class ConsoleExercises {
         System.out.printf("Nice to meet you %s %n", nameInput);
         input.close();
         //adding contribution because im not getting mine for some reason
-
-
-
     }
 }
